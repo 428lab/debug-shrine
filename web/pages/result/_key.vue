dfa4af16c89688496e9e42a0521d8577cc318b60
penginnu <i.link0502@gmail.com>
2021-12-09 18:58:19 +0900

    [add]おみくじ内容をKeyに応じて変更するよう機能追加
    外部jsonファイル内のおみくじ内容を参照するように仮実装

--- web/pages/result/_key.vue
+++ web/pages/result/_key.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="text-center">
     <p>
-    大吉<br>
-    今日はいいことがあるかも
+    {{ title }}<br>
+    {{ message }}
+    </p>
+    <p>
+      {{ omikujiNo }}
     </p>
     <div>SNSで結果をシェア</div>
     <button>Twitter</button>
@@ -10,3 +13,30 @@
     <button>LINE</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      message: "",
+      omikujiNo: 0
+    }
+  },
+  mounted() {
+    // おみくじID取得
+    const omikujiId = this.$route.params.key;
+
+    // おみくじデータ読み込み
+    const omikujiData = require('~/assets/json/omikuji.json');
+
+    // おみくじ番号の生成
+    const omikujiNo = omikujiId % omikujiData.length;
+    console.log(omikujiData[omikujiNo].title);
+
+    this.title = omikujiData[omikujiNo].title;
+    this.message = omikujiData[omikujiNo].message;
+    this.omikujiNo = omikujiNo;
+  },
+}
+</script>
