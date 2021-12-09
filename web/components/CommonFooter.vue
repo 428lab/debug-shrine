eb69c09627c51b42429c1e68be4b8824aae4d58d
penginnu <i.link0502@gmail.com>
2021-12-09 19:05:02 +0900

    [fix]リンクミスの修正
    利用規約に正しくリンクするように修正

--- web/components/CommonFooter.vue
+++ web/components/CommonFooter.vue
@@ -3,7 +3,7 @@
     <div class="text-center text-white p-3 mt-5 bg-dark">
       <div class="container">
         <div class="d-md-flex justify-content-center">
-          <nuxt-link class="p-3" to="/term">
+          <nuxt-link class="p-3" to="/terms">
             利用規約
           </nuxt-link>
           <nuxt-link class="p-3" to="/policy">
