59170f74e4b88f4cd76d3b7173703fc536b4490b
penginnu <i.link0502@gmail.com>
2021-12-10 21:45:15 +0900

    [add]MyPageにレーダーチャート表示機能の追加
    Myapage内「つよさ」にレーダーチャートを追加。一部コメント追記。

---
+++ web/components/charts/powerChart.vue
@@ -0,0 +1,11 @@
+<script>
+import { Radar } from 'vue-chartjs';
+
+export default {
+  extends: Radar,
+  props:['chartData', 'options'],
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+}
+</script>
--- web/pages/mypage.vue
+++ web/pages/mypage.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container">
-    <div class="bg-light p-5 rounded row align-items-center">
+    <div class="bg-light p-5 row align-items-center">
       <div class="col border-end">
         <div class="row">
           <div class="col-3">
@@ -19,16 +19,17 @@
         <p class="text-end w-100">NEXT 2234EXP</p>
       </div>
     </div>
-    <div class="row px-2 my-2">
-      <div class="col bg-light p-2 rounded mx-2">
+    <div class="row my-2 gap-2">
+      <div class="col-3 bg-light p-2 rounded">
         <h5>つよさ</h5>
+        <RadarChart :chartData="chartData" :options="options" />
       </div>
-      <div class="col bg-light p-2 rounded mx-2">
+      <div class="col bg-light p-2 rounded">
         <h5>ポイント獲得履歴</h5>
       </div>
     </div>
-    <div class="row px-2 my-2">
-      <div class="col bg-light p-2 rounded mx-2">
+    <div class="row my-2">
+      <div class="col bg-light p-2 rounded">
         <h5>アクティビティ</h5>
       </div>
     </div>
@@ -37,13 +38,41 @@
 
 <script>
 import { getAuth, onAuthStateChanged, ProviderId } from 'firebase/auth';
+import RadarChart from '@/components/charts/powerChart.vue';
 
 export default {
+  components: { RadarChart },
   data() {
     return {
       userName: "",
       userNickname: "",
-      photoURL: ""
+      photoURL: "",
+      chartData: {
+        labels: ["ちから", "たいりょく", "しゅびりょく", "きようさ", "すばやさ", "かしこさ"],
+        datasets: [{
+          type: "radar",
+          data: [50, 30, 40, 20, 45, 25],
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }]
+      },
+      options: {
+        pointLabels: {
+          display: false
+        },
+        scale: {
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            max: 100
+          }
+        }
+      }
     }
   },
   mounted() {
@@ -55,11 +84,15 @@ export default {
       if ( user ) {
         // サインインしている場合
         user.providerData.forEach((profile) => {
+          // プロバイダーデータの取得
           if (profile.providerId == "github.com") {
             this.userName = profile.displayName;
             this.photoURL = profile.photoURL;
           }
         });
+      } else {
+        // 非ログイン時 トップページへリダイレクト
+        redirect('/');
       }
     });
   }
