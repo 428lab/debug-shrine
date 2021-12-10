6ef66ea62e400692307a98651bdf4a541c695858
penginnu <i.link0502@gmail.com>
2021-12-10 15:33:19 +0900

    [add]MyPageの仮デザイン実装
    Mypageにデザインを追加

---
+++ web/pages/mypage.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="container">
+    <div class="bg-light p-5 rounded row align-items-center">
+      <div class="col border-end">
+        <div class="row">
+          <div class="col-3">
+            <img src="http://placehold.it/460x460" alt="userName" class="w-100">
+          </div>
+          <div class="col-9">
+            <p class="fs-3">userNickname</p>
+            <p class="fs-4">userName</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <p class="fs-5">RANK 12</p>
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <p class="text-end w-100">NEXT 2234EXP</p>
+      </div>
+    </div>
+    <div class="row px-2 my-2">
+      <div class="col bg-light p-2 rounded mx-2">
+        <h5>つよさ</h5>
+      </div>
+      <div class="col bg-light p-2 rounded mx-2">
+        <h5>ポイント獲得履歴</h5>
+      </div>
+    </div>
+    <div class="row px-2 my-2">
+      <div class="col bg-light p-2 rounded mx-2">
+        <h5>アクティビティ</h5>
+      </div>
+    </div>
+  </main>
+</template>
