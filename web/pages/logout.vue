84ba9f082ccd9a3ae63aa295332f8aca9932a5e0
penginnu <i.link0502@gmail.com>
2021-12-12 17:57:36 +0900

    [add]検証用にログアウト機能の実装

---
+++ web/pages/logout.vue
@@ -0,0 +1,16 @@
+<script>
+import { getAuth, signOut } from 'firebase/auth';
+
+export default {
+  mounted() {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful
+      console.log("LOGOUT");
+
+      // redirect
+      this.$router.push('/');
+    });
+  }
+}
+</script>
