1c50c17a47944d70e309da609fb9f182a5de83b8
penginnu <i.link0502@gmail.com>
2021-12-14 21:10:43 +0900

    [change] チャートオプションをコンポーネント側に変更
    チャートのオプションをpage側からcomponent側で管理するように変更

--- web/components/charts/powerChart.vue
+++ web/components/charts/powerChart.vue
@@ -3,7 +3,39 @@ import { Radar } from 'vue-chartjs';
 
 export default {
   extends: Radar,
-  props:['chartData', 'options'],
+  data: function() {
+    return {
+      options: {
+        animation: {
+          // アニメーション実行時間（ms）
+          duration: 1000,
+          // イージング指定（https://easings.net/）
+          easing: "easeInOutCirc"
+        },
+        legend: {
+          // 凡例を表示しない
+          display: false
+        },
+        tooltips: {
+          // ツールチップを表示しない
+          enabled: false
+        },
+        scale: {
+          ticks: {
+            // メモリ線を表示しない
+            display: false,
+            // 0からの表示を有効可
+            beginAtZero: true,
+            // 最小値を0に固定
+            min: 0,
+            // 最大値を100に固定
+            max: 100
+          }
+        }
+      }
+    }
+  },
+  props:['chartData'],
   mounted() {
     this.renderChart(this.chartData, this.options);
   }
--- web/pages/dashboard/index.vue
+++ web/pages/dashboard/index.vue
@@ -36,7 +36,7 @@
         <div class="row my-2 gap-2">
           <div class="col-4 bg-light p-3 rounded">
             <h5>つよさ</h5>
-            <RadarChart :chartData="chartData" :options="options" />
+            <RadarChart :chartData="chartData" />
           </div>
           <div class="col bg-light p-3 rounded">
             <h5>ポイント獲得履歴</h5>
@@ -108,34 +108,6 @@ export default {
           pointHoverBackgroundColor: '#fff',
           pointHoverBorderColor: 'rgb(255, 99, 132)'
         }]
-      },
-      options: {
-        animation: {
-          // アニメーション実行時間（ms）
-          duration: 1000,
-          // イージング指定（https://easings.net/）
-          easing: "easeInOutCirc"
-        },
-        legend: {
-          // 凡例を表示しない
-          display: false
-        },
-        tooltips: {
-          // ツールチップを表示しない
-          enabled: false
-        },
-        scale: {
-          ticks: {
-            // メモリ線を表示しない
-            display: false,
-            // 0からの表示を有効可
-            beginAtZero: true,
-            // 最小値を0に固定
-            min: 0,
-            // 最大値を100に固定
-            max: 100
-          }
-        }
       }
     }
   },
