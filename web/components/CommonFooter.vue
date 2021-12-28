7108cad7e8d88655ad62214c68dcbcbdb88e2575
penginnu <i.link0502@gmail.com>
2021-12-28 12:52:52 +0900

    [update] フッターデザインの調整

--- web/components/CommonFooter.vue
+++ web/components/CommonFooter.vue
@@ -1,31 +1,61 @@
 <template>
-  <div>
-    <div class="py-5"></div>
-    <div class="text-center text-white p-3 mt-5 bg-dark fixed-bottom">
-      <div class="container">
-        <div class="d-md-flex justify-content-center">
-          <nuxt-link class="p-2 footer-link" to="/terms"> 利用規約 </nuxt-link>
-          <nuxt-link class="p-2 footer-link" to="/privacy-policy"> プライバシーポリシー </nuxt-link>
+  <div class="bg-dark">
+    <div class="container-lg">
+      <div class="py-4 text-center">
+        <!-- ロゴ -->
+        <div class="py-2">
+          <img src="https://placehold.jp/300x100.png?text=logo" alt="でばっぐ神社">
         </div>
-        <div class="mt-3">
-          Copyright 2021 -
-          <a href="https://428lab.net" target="_blank">四谷ラボ</a>
+        <!-- フッターナビゲーション -->
+        <nav id="footer-nav" class="container-sm py-2">
+          <ul>
+            <li class="px-2"><nuxt-link to="/">トップ</nuxt-link></li>
+            <li class="px-2"><nuxt-link to="/">利用規約</nuxt-link></li>
+            <li class="px-2"><nuxt-link to="/">プライバシーポリシー</nuxt-link></li>
+          </ul>
+        </nav>
+        <!-- SNSリンク -->
+        <div class="py-2">
+          <img src="https://placehold.jp/300x50.png?text=logo" alt="でばっぐ神社">
         </div>
       </div>
     </div>
-    <!-- <div class="text-center text-white bg-dark p-3 fixed-bottom-md">
-      <div class="container">
-
-        <div>
-          Copyright 2021 - 四谷ラボ
-        </div>
+    <div class="footer-rights">
+      <div class="py-2 text-right container-lg">
+        &copy; 2021 428lab.
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <style>
-.footer-link {
-  font-size: 0.9rem;
+nav#footer-nav > ul {
+  list-style-type: none;
+  list-style-position: outside;
+  font-size: 0.8rem;
+  margin: 0;
+  padding: 0;
+}
+
+nav#footer-nav > ul > li {
+  display: inline-block;
+  text-decoration: none;
+}
+
+nav#footer-nav > ul > li+ li {
+  border-left: 1px solid #adb5bd;
+}
+
+nav#footer-nav > ul > li > a {
+  text-decoration: none;
+}
+
+.footer-rights {
+  background-color: black;
+}
+
+.footer-rights > div {
+  font-size: 0.8rem;
+  text-align: right;
 }
 </style>
