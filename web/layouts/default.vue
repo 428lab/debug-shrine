2807055a9b1efc19ac76dbf1bb577b0f9a024cbc
penginnu <i.link0502@gmail.com>
2021-12-28 20:09:50 +0900

    [update] 全体のレイアウト修正
    フッター部分の描画位置の調整など

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <CommonHeader />
-    <Nuxt />
-    <CommonFooter />
+    <main>
+      <Nuxt />
+    </main>
+    <footer>
+      <CommonFooter />
+    </footer>
   </div>
 </template>
+
+<style>
+main {
+  min-height: calc(100vh - 300px);
+}
+</style>
