5ad9ea4d781ed182135e34c66000d639d6462a6b
T.Shinohara <takuma1201@gmail.com>
2021-12-29 22:29:50 +0900

    参拝ページの実装

--- web/pages/sanpai.vue
+++ web/pages/sanpai.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="text-center">
-    <div class="container py-5">
+    <div class="container py-5" v-if="status">
       <h1>参拝ありがとう！</h1>
       <h2>ポイントを獲得しました</h2>
-      <h2>＋20 exp</h2>
-      <p class="fs-5">LEVEL {{ 20 }}</p>
+      <h2>＋{{ status.exp.get }} exp</h2>
+      <p class="fs-5">LEVEL {{ status.level }}</p>
       <div class="progress">
         <div
           class="progress-bar p-2"
           role="progressbar"
-          style="width: 30%"
-          aria-valuenow="10"
+          :style="`width:` + (status.exp.total / status.exp.next) * 100 + `%`"
+          :aria-valuenow="status.exp.total"
           aria-valuemin="0"
-          aria-valuemax="100"
+          :aria-valuemax="status.exp.next"
         >
-          {{ 20 }}exp
+          {{ status.exp.total }}exp
         </div>
       </div>
-      <p class="text-end w-100">NEXT 2234EXP</p>
+      <p class="text-end w-100">NEXT {{ status.exp.next }}exp</p>
     </div>
     <!-- <button class="btn btn-lg btn-primary" @click="sanpai">
       マイページを見る
@@ -35,18 +35,30 @@
 import { mapGetters } from "vuex";
 
 export default {
-  async asyncData({ $axios, store }) {
-    console.log(store.getters['user']);
-    let payload = {
-      github_id: store.getters['user'].github_id,
-    }
-    response = await $axios.post("sanpai", payload);
-    console.log(response);
+  data() {
     return {
       isLoading: true,
+      status: {
+        level: 0,
+        exp: {
+          next: 0,
+          get: 0,
+          total: 0,
+        },
+      },
     };
   },
-  mounted() {},
+  async mounted() {
+    let payload = {
+      github_id: this.user.github_id,
+    };
+    let response = await this.$axios.post("sanpai", payload);
+    this.status.level = response.data.level;
+    this.status.exp.next = response.data.next_exp;
+    this.status.exp.get = response.data.add_exp;
+    this.status.exp.total = response.data.exp;
+    this.isLoading = false;
+  },
   methods: {
     sanpai() {
       this.$router.push("/result/" + "0123456789");
