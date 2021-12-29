d8123096713b1cb011664e9192600d308ec18c72
T.Shinohara <takuma1201@gmail.com>
2021-12-29 22:11:55 +0900

    vuex-persistedstateの実装

--- web/plugins/persistedstate.js
+++ web/plugins/persistedstate.js
@@ -1,9 +1,5 @@
 import createPersistedState from 'vuex-persistedstate'
 
 export default ({ store }) => {
-  window.onNuxtReady(() => {
-    createPersistedState({
-      key: 'debug-shrine'
-    })(store)
-  })
+  createPersistedState({ key: 'debug-shrine' })(store)
 }
