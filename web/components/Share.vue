47e23b03ce501c4b1d63a5e3ae905d94fa7492ef
T.Shinohara <takuma1201@gmail.com>
2021-12-30 22:00:44 +0900

    シェアリンク修正

--- web/components/Share.vue
+++ web/components/Share.vue
@@ -10,30 +10,41 @@
         <img src="/brandlogo/facebook.svg" alt="" width="24px" />
         <span class="ms-2"> Facebook </span>
       </a>
-      <a :href="lineUrl" class="btn text-white bg-line" target="_blank">
+      <!-- <a :href="lineUrl" class="btn text-white bg-line" target="_blank">
         <img src="/brandlogo/line.svg" alt="" width="24px" />
         <span class="ms-2"> LINE </span>
-      </a>
+      </a> -->
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   props: {
     title: "",
     url: "",
   },
   computed: {
+    ...mapGetters(["user"]),
     twitterUrl() {
-      return this.url;
+      return (
+        "https://twitter.com/share?url=" +
+        encodeURIComponent(this.url) +
+        "&text=" +
+        encodeURIComponent(
+          "これが" + this.user.display_name + "の でばっぐのうりょくだ！"
+        ) +
+        "&hashtags=でばっぐ神社"
+      );
     },
     facebookUrl() {
-      return this.url;
+      return "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.url);
     },
-    lineUrl() {
-      return this.url;
-    },
+    // lineUrl() {
+    //   return this.url;
+    // },
   },
 };
 </script>
