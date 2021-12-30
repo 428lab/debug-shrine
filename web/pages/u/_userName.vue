69c14cf8065ccaf09ca968c9bdbfc8fe69437194
T.Shinohara <takuma1201@gmail.com>
2021-12-30 16:26:58 +0900

    ユーザーページのレイアウト修正

--- web/pages/u/_userName.vue
+++ web/pages/u/_userName.vue
@@ -1,29 +1,56 @@
 <template>
   <main class="container p-3">
     <div class="p-3 profile-outline">
-      <div class="bg-dark p-4 ">
-        <div>
-          <span class="">{{ user.nickName }}</span>
-          <span class="ms-3">{{ user.screenName }}</span>
-        </div>
-        <div class="flex mt-3">
-          <img :src="user.profileImage" alt="" class="w-35 img-fluid" />
-        </div>
-        <div class="mt-3">
-          <div>れべる：{{ status.level }}</div>
-          <div>ポイント：{{ status.points }}</div>
-          <div>せんとうりょく：{{ status.total }}</div>
-        </div>
-      </div>
-      <div class="row mt-4">
-        <div class="col-8">
-          <div class="bg-primary text-center d-inline-block p-2 debug-title">
-            <small>でばっぐ<br />のうりょく</small>
+      <div class="row">
+        <div class="col-12 col-md-5 col-xl-8 mb-4 mb-md-0">
+          <div class="p-3 bg-dark h-100 rounded">
+            <div class="d-lg-flex align-items-center">
+              <div class="fs-4 me-4">{{ user.nickName }}</div>
+            </div>
+            <div class="d-flex mt-3">
+              <div class="w-35">
+                <img
+                  :src="user.profileImage"
+                  alt=""
+                  class="rounded-icon img-fluid w-100"
+                />
+              </div>
+              <div class="ms-4 flex-fill">
+                <a
+                  :href="`https://github.com/` + user.screenName"
+                  class="d-flex align-items-center"
+                >
+                  <img src="/brandlogo/github.svg" height="20px" alt="" />
+                  <span class="ms-2">{{ user.screenName }} ></span>
+                </a>
+                <div class="mt-3">れべる：{{ status.level }}</div>
+                <div>ポイント：{{ status.points }}</div>
+                <div>せんとうりょく：{{ status.total }}</div>
+              </div>
+            </div>
           </div>
-          <RadarChart :chartData="chartData" />
         </div>
-        <div class="col-4 align-items-center">
-          <img src="/profile_parts.png" class="img-fluid" alt="" />
+        <div class="col-12 col-md-7 col-xl-4">
+          <div class="row">
+            <div class="d-none d-md-block col-12">
+
+            </div>
+            <div class="col-8 col-md-12">
+              <div
+                class="bg-primary text-center d-inline-block p-2 debug-title"
+              >
+                <small>でばっぐ<br class="d-md-none"/>のうりょく</small>
+              </div>
+              <RadarChart :chartData="chartData" />
+            </div>
+            <div class="col-4 align-items-center d-md-none">
+              <img
+                src="/ProfileParts/profile_parts.png"
+                class="img-fluid"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
