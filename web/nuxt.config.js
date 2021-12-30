5d4fbeed00e79d65cb7b944020cb8064a586c3e7
zinntikumugai <uesitananame55@gmail.com>
2021-12-31 00:49:23 +0900

    サイトOG画像仮適応

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -16,7 +16,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:image', property: 'og:image', content: `/shrine.png`}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
