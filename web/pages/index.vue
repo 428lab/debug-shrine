2a44651afe9482d2aed6246ae4cfbe01091b3706
T.Shinohara <takuma1201@gmail.com>
2021-12-31 13:25:27 +0900

    インデックスのレイアウト修正対応

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,60 +1,65 @@
 <template>
   <div class="text-center">
-    <div class="container pt-4">
-      <div class="p-5">
-        <img
-          src="/torii.svg"
-          alt="でばっぐ神社"
-          class="w-100"
-          style="max-width: 700px"
-        />
-        <div class="text-end mt-4" style="max-width: 700px">
-          <nuxt-link to="/about" class="">でばっぐ神社とは ></nuxt-link>
+    <div class="container py-5 justify-content-center">
+      <div class="row">
+        <div class="col-2 col-md-3 col-lg-4"></div>
+        <div class="col-8 col-md-6 col-lg-4">
+          <div class="">
+            <img src="/torii.svg" alt="でばっぐ神社" class="w-100" style="" />
+            <div class="text-end mt-4" style="">
+              <nuxt-link to="/about" class="">でばっぐ神社とは ></nuxt-link>
+            </div>
+          </div>
         </div>
+        <div class="col-2 col-md-3 col-lg-4"></div>
       </div>
     </div>
     <div class="bg-github py-4">
       <div class="container">
-        <div class="row align-items-center">
-          <div class="col-2"></div>
-          <div class="col-3 resizeimage">
-            <img src="/brandlogo/github.svg" class="img-fluid" />
+        <div class="row py-3">
+          <div class="col-2 col-md-3 col-lg-4"></div>
+          <div class="col-8 col-md-6 col-lg-4">
+            <div class="row align-items-center">
+              <div class="col-4 resizeimage">
+                <img src="/brandlogo/github.svg" class="img-fluid" />
+              </div>
+              <div class="col-4 resizeimage">
+                <img src="/activity.svg" class="img-fluid w-75" />
+              </div>
+              <div class="col-4 resizeimage">
+                <img src="/shrine.png" class="img-fluid" />
+              </div>
+            </div>
           </div>
-          <div class="col-2 resizeimage">
-            <img src="/activity.svg" class="img-fluid" />
-          </div>
-          <div class="col-3 resizeimage">
-            <img src="/shrine.png" class="img-fluid" />
-          </div>
-          <div class="col-2"></div>
+          <div class="col-2 col-md-3 col-lg-4"></div>
         </div>
         <div class="mt-4">
-          <button @click="GitHubAuth" class="btn btn-lg btn-primary" :disabled="buttons.sanpai">
+          <button
+            @click="GitHubAuth"
+            class="btn btn-lg btn-primary"
+            :disabled="buttons.sanpai"
+          >
             <template v-if="!isLogin">
               GitHubと連携して<br class="d-md-none" />
             </template>
             参拝する
           </button>
         </div>
-        <div v-if="isLogin" class="mt-4 p-2 d-inline-block text-end">
-          <div class="rounded border p-2 d-inline-block">
+        <div v-if="isLogin" class="mt-4 p-2 d-inline-block">
+          <div class="rounded border p-2 w-100 mb-2" v-if="isLogin">
             <img
               :src="user.image_path"
               class="rounded-icon img-fluid"
-              width="24px"
+              width="30px"
             />
             {{ user.display_name }} でログイン中
           </div>
-          <div class="mt-4 d-flex justify-content-between">
-            <div>
-              <a href="javascript:void(0)" class="" @click="logout"
-                >ログアウト</a
-              >
-            </div>
-            <div>
-              <nuxt-link to="/dashboard">マイページへ ></nuxt-link>
-            </div>
-          </div>
+          <a href="javascript:void(0)" class="btn btn-secondary" @click="logout"
+            >ログアウト</a
+          >
+          <nuxt-link to="/dashboard" class="btn text-white"
+            >マイページへ ></nuxt-link
+          >
         </div>
       </div>
     </div>
