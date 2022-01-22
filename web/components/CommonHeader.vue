343387c1cb631031cd4f852d7bda3c89fc33788e
T.Shinohara <takuma1201@gmail.com>
2022-01-22 14:33:32 +0900

    ハンバーガーメニューの復活

--- web/components/CommonHeader.vue
+++ web/components/CommonHeader.vue
@@ -8,21 +8,42 @@
           class="me-2"
         />でばっぐ神社</nuxt-link
       >
-      <div class="d-flex nav-item" v-if="isLogin">
-        <nuxt-link class="nav-link active" aria-current="page" to="/dashboard"
-          >マイページ</nuxt-link
-        >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <nuxt-link class="navbar-brand" to="/">Home</nuxt-link>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li> -->
+        </ul>
+        <ul class="navbar-nav mb-2 mb-lg-0" v-if="isLogin">
+          <li class="nav-item">
+            <nuxt-link
+              class="nav-link active"
+              aria-current="page"
+              to="/dashboard"
+              >マイページ</nuxt-link
+            >
+          </li>
+          <li class="nav-item">
+            <button class="btn btn-secondary" @click="logout">
+              ログアウト
+            </button>
+          </li>
+        </ul>
       </div>
-      <!-- <ul class="navbar-nav mb-2 mb-lg-0" v-if="isLogin">
-        <li class="nav-item">
-          <nuxt-link class="nav-link active" aria-current="page" to="/dashboard"
-            >マイページ</nuxt-link
-          >
-        </li>
-        <li class="nav-item">
-          <button class="btn btn-secondary" @click="logout">ログアウト</button>
-        </li>
-      </ul> -->
     </div>
   </nav>
 </template>
